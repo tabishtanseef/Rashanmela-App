dcaa722f5c4cce269b55e04ee41521abef106f29
Tabish Tanseef <tabishtanseef@gmail.com>
2020-07-23 17:00:21 +0530

    Adding the app to github

---
+++ app/src/main/java/com/RashanMela/rashanmela/MainActivity.java
@@ -0,0 +1,45 @@
+package com.RashanMela.rashanmela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN=4000;
+    Animation topAnim, bottomAnim, slideAnim;
+    ImageView image, logo;
+    TextView slogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        slideAnim = AnimationUtils.loadAnimation(this,R.anim.slide_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.textView);
+
+        image.setAnimation(slideAnim);
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
